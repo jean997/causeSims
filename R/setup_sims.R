2d091fa72f963ee9b203c130c6d4b0d1e4299f9e
Jean Morrison <jeanm@uchicago.edu>
2019-07-24 19:45:49 -0500

    add dowload results and dowload code

--- R/setup_sims.R
+++ R/setup_sims.R
@@ -4,9 +4,11 @@
 #'(https://www.biorxiv.org/content/10.1101/682237v1). After running this function you will be able to execute simulation
 #'experiments using DSC using the commands
 #'
-#'  nohup dsc --replicate 100 --host config.yml -c 4 pwr.dsc > pwr_dsc.out &
+#'  nohup dsc --replicate 100 --host config.yml -c 4 power.dsc > pwr.out &
 #'
-#'  nohup dsc --replicate 100 --host config.yml -c 4 false_positives.dsc > fp_dsc.out &
+#'  nohup dsc --replicate 100 --host config.yml -c 4 false_positives.dsc > fp.out &
+#'
+#'  nohup dsc --replicate 100 --host config.yml -c 4 sigma_g.dsc > sg.out &
 #'
 #'  You will need to modify the config.yml file to work with your compute cluster.
 #'@export
@@ -15,6 +17,7 @@ setup_sims <- function(){
   cat("Downloading DSC files\n")
   download.file(url = "https://raw.githubusercontent.com/jean997/causeSims/master/dsc_files/power.dsc" , destfile="power.dsc")
   download.file(url = "https://raw.githubusercontent.com/jean997/causeSims/master/dsc_files/false_positives.dsc" , destfile="false_positives.dsc")
+  download.file(url = "https://raw.githubusercontent.com/jean997/causeSims/master/dsc_files/sigma_g.dsc" , destfile="sigma_g.dsc")
 
   #Download data
   if(!dir.exists("data/")) system("mkdir data")
@@ -24,4 +27,18 @@ setup_sims <- function(){
 
   cat("Downloading config file. You will need to modify this for your setup\n")
   download.file(url="https://raw.githubusercontent.com/jean997/causeSims/master/dsc_files/config.yml", destfile="config.yml")
+
+  #Download Results
+  cat("Downloading Results\n")
+  if(!dir.exists("sim_results/")) system("mkdir sim_results")
+  download.file(url = "https://zenodo.org/record/3350911/files/main_causedf.RDS?download=1", destfile="sim_results/main_causedf.RDS")
+  download.file(url = "https://zenodo.org/record/3350911/files/main_lcvdf.RDS?download=1", destfile="sim_results/main_lcvdf.RDS")
+  download.file(url = "https://zenodo.org/record/3350911/files/main_mrdf.RDS?download=1", destfile="sim_results/main_mrdf.RDS")
+  download.file(url = "https://zenodo.org/record/3350911/files/sg_causedf.RDS?download=1", destfile="sim_results/sg_causedf.RDS")
+
+  #Download code
+  cat("Downloading Code\n")
+  download.file(url="https://raw.githubusercontent.com/jean997/cause/master/code/extract_results_main.R", destfile="extract_results_main.R")
+  download.file(url="https://raw.githubusercontent.com/jean997/cause/master/code/extract_sigma_g.R", destfile="extract_sigma_g.R")
+  download.file(url="https://raw.githubusercontent.com/jean997/cause/master/code/summarize_sigma_g.R", destfile="summarize_sigma_g.R")
 }
