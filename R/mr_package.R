f9ff23a5aec2937411e3d571090843b8dfc89f6e
Jean Morrison <jeanm@uchicago.edu>
2019-09-23 14:33:25 -0500

    add functions using MendelianRandomization package

---
+++ R/mr_package.R
@@ -0,0 +1,57 @@
+
+#'@export
+ivw_re_nonome_MR <- function(dat, p_val_thresh=5e-8, no_ld = FALSE){
+  if(no_ld) dat <- process_dat_nold(dat)
+  dat <- dat %>% filter(p_value < p_val_thresh & ld_prune == TRUE)
+  if(nrow(dat) < 2){
+    R <- list("z"=NA, "p" = NA)
+    return(R)
+  }
+  f <- mr_ivw(dat, model="random", weights="delta")
+  R <- list(z = f@Estimate/f@StdError, p = f@Pvalue)
+  return(R)
+}
+
+#'@export
+egger_re_MR <- function(dat, p_val_thresh=5e-8, no_ld = FALSE){
+  if(no_ld) dat <- process_dat_nold(dat)
+  dat <- dat %>% filter(p_value < p_val_thresh & ld_prune == TRUE)
+  if(nrow(dat) < 2){
+    R <- list("z"=NA, "p" = NA)
+    return(R)
+  }
+  f <- mr_egger(dat)
+  R <- list(z = f@Estimate/f@StdError.Est, p = f@Pvalue.Est)
+  return(R)
+}
+
+#'@export
+wtd_median_MR <- function(dat, p_val_thresh=5e-8, no_ld = FALSE){
+  if(no_ld) dat <- process_dat_nold(dat)
+  dat <- dat %>% filter(p_value < p_val_thresh & ld_prune == TRUE)
+  if(nrow(dat) < 2){
+    R <- list("z"=NA, "p" = NA)
+    return(R)
+  }
+  f <- mr_median(dat)
+  R <- list(z = f@Estimate/f@StdError, p = f@Pvalue)
+  return(R)
+}
+
+#'@export
+mbe_MR <- function(dat, p_val_thresh=5e-8, no_ld = FALSE,
+                   weighting="weighted", no_NOME=TRUE, phi=1){
+  if(no_ld) dat <- process_dat_nold(dat)
+  dat <- dat %>% filter(p_value < p_val_thresh & ld_prune == TRUE)
+  if(nrow(dat) < 2){
+    R <- list("z"=NA, "p" = NA)
+    return(R)
+  }
+  if(no_NOME) stderror <- "delta"
+    else stderror <- "simple"
+  f <- mr_mbe(dat, weighting=weighting, stderror=stderror, phi=phi)
+  R <- list(z = f@Estimate/f@StdError, p = f@Pvalue)
+  return(R)
+}
+
+
