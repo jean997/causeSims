60e05e10e8ec628ddd1a424162726304332d505d
Jean Morrison <jeanm@uchicago.edu>
2019-12-09 15:16:51 -0600

    add arg for max_candidates

--- R/cause_params_sims.R
+++ R/cause_params_sims.R
@@ -5,7 +5,7 @@
 #'@param no_ld Run with the nold data (T/F)
 #'@return
 #'@export
-cause_params_sims <- function(dat, null_wt = 10, no_ld=FALSE){
+cause_params_sims <- function(dat, null_wt = 10, no_ld=FALSE, max_candidates=Inf){
 
     if(no_ld){
       dat <- dat %>% select(-beta_hat_1, -beta_hat_2) %>%
@@ -16,7 +16,7 @@ cause_params_sims <- function(dat, null_wt = 10, no_ld=FALSE){
          select(snp, beta_hat_1, seb1, beta_hat_2, seb2) %>%
          new_cause_data(.)
 
-    params <- est_cause_params(X, X$snp, null_wt = null_wt)
+    params <- est_cause_params(X, X$snp, null_wt = null_wt, max_candidates = max_candidates)
     return(params)
 }
 
