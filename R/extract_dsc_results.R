1997e0ac3e55a23ac386b0b4911daba1fe9979c1
Jean Morrison <jeanm@uchicago.edu>
2020-09-02 15:41:32 -0400

    remove some unused code bits, fix a typo

--- R/extract_dsc_results.R
+++ R/extract_dsc_results.R
@@ -1,29 +1,9 @@
 
 #'@export
 extract_dsc_results <- function(dir, extract_cause=TRUE, extract_mr = TRUE,
-                                extract_lcv = TRUE, sigma_g = FALSE, version1 = FALSE){
+                                extract_lcv = TRUE, sigma_g = FALSE){
 
-  if(version1){
-    paramdf <- dscquery(dsc.outdir=dir,
-                        targets=c("simulate",
-                                  "gw_sig.tau","gw_sig.gamma", "gw_sig.q",
-                                  "gw_sig.omega", "gw_sig.eta", "gw_sig.h1", "gw_sig.h2",
-                                  "gw_sig.n1", "gw_sig.n2", "gw_sig.neffect1",
-                                  "gw_sig.neffect2", "gw_sig.lcv_q1", "gw_sig.lcv_q2",
-                                  "gw_sig.lcv_gcp",  "gw_sig.m_sig") ,
-                        module.output.files = c("simulate"),
-                        return.type="data.frame")
-    paramdf <- paramdf %>% rename(q = gw_sig.q, tau = gw_sig.tau,
-                                  eta = gw_sig.eta, gamma = gw_sig.gamma,
-                                  omega = gw_sig.omega, h1 = gw_sig.h1,
-                                  h2 = gw_sig.h2, n1 = gw_sig.n1, n2 = gw_sig.n2,
-                                  neffect1 = gw_sig.neffect1, neffect2 = gw_sig.neffect2,
-                                  m_sig = gw_sig.m_sig,
-                                  lcv_q1 = gw_sig.lcv_q1, lcv_q2 = gw_sig.lcv_q2,
-                                  gcp = gw_sig.lcv_gcp) %>%
-      mutate(params = paste0("(", tau, ",", omega, ",", q, ")"))
-    res <- list(params = paramdf)
-  }else{
+
     paramdf <- dscquery(dsc.outdir=dir,
                      targets=c("simulate",
                                "data_summ.tau","data_summ.gamma", "data_summ.q",
@@ -43,7 +23,6 @@ extract_dsc_results <- function(dir, extract_cause=TRUE, extract_mr = TRUE,
                                 gcp = data_summ.lcv_gcp) %>%
                   mutate(params = paste0("(", tau, ",", omega, ",", q, ")"))
     res <- list(params = paramdf)
-  }
 
 
   #cause
@@ -62,7 +41,7 @@ extract_dsc_results <- function(dir, extract_cause=TRUE, extract_mr = TRUE,
       causedf <- causedf %>%
                  full_join(paramdf, ., by=c("DSC", "simulate.output.file"))
       res[["cause"]] <- causedf
-  }else if(extract_case & sigma_g){
+  }else if(extract_cause & sigma_g){
     causedf <- dscquery(dsc.outdir=dir,
                         targets=c("simulate", "cause", "cause.qbeta", "cause.quant", "cause.sigma_g",
                                   "cause.p", "cause.eta_2_med", "cause.q_2_med",
@@ -78,8 +57,6 @@ extract_dsc_results <- function(dir, extract_cause=TRUE, extract_mr = TRUE,
                 full_join(paramdf, ., by=c("DSC", "simulate.output.file"))
      res[["cause"]] <- causedf
   }
-
-
 
 # mr
   if(extract_mr){
